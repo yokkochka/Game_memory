f79eabe8d647c61677b598958d42995249183840
Ulyana Yunusova <85567113+Ulyanaaa@users.noreply.github.com>
2024-08-17 22:38:18 +0300

    Changed README.md, refactor code

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# Game_memory
-Игра "Память" (карточки)
+# Memory Game with Pygame
+### Pet project (autumn 2023)
+
+This repository contains a memory game built using the Pygame library. The game challenges players to match pairs of cards with various shapes and colors. It includes a leaderboard and animations for level transitions and victories.
+
+## Description
+
+The game presents a grid of cards face down. Players take turns flipping two cards at a time, trying to find matching pairs. The game includes different levels with increasing difficulty, and it tracks the player's score and remaining steps. Upon completing a level, the game advances to a new level with a larger grid. The player can view their score and name on a leaderboard.
+
+## Features
+
+- **Multiple levels**: Increase the grid size and difficulty with each new level.
+- **Card matching**: Flip and match pairs of cards based on color and shape.
+- **Score tracking**: Keep track of the player's score and remaining steps.
+- **Victory animation**: Display an animation when the player completes a level.
+- **Leaderboard**: Save and display the top scores.
+- **User input**: Enter a name to save to the leaderboard.
+- **Game restart**: Restart the game or return to the leaderboard upon completion.
+
+## Usage
+
+1. **Start the game**: Run the script to start the game.
+2. **Play the game**: Click on cards to flip them and try to match pairs.
+3. **View leaderboard**: When the game ends, you can view the leaderboard and enter your name to save your score.
+4. **Restart or quit**: Choose to restart the game or quit from the leaderboard screen.
+
+## Installation and running
+
+1. **Clone the repository:**
+    ```bash
+    git clone https://github.com/your_username/your_repository.git
+    ```
+
+2. **Navigate to the project directory:**
+    ```bash
+    cd your_repository
+    ```
+
+3. **Ensure required modules are installed:**
+    Install the necessary modules using pip:
+    ```bash
+    pip install pygame
+    ```
+
+4. **Run the script:**
+
+## Screenshots
+
+### Main Game Screen
+![Game Screen](./screenshots/game_screen.png)
+
+## File Structure
+
+- `game_memory.py`: Main script file for the game.
+- `leaderboard.txt`: File to store top scores.
+- `music_dir/`: Directory containing the music file for the game.
+- `materials/`: Directory for screenshots of the game.
+
--- game_memory.py
+++ game_memory.py
@@ -10,9 +10,8 @@ class Game:
         self.WIN_WIDTH = 640 
         self.WIN_HEIGHT = 480
 
-        self.FPS = 30   # Переменная для фпс
+        self.FPS = 30  
 
-        # Цвета
         self.LIGHTBLUE = (151, 157, 226)
         self.YELLOW = (218, 230, 112)
         self.SKINCOLOR = (218, 177, 124)
@@ -30,57 +29,46 @@ class Game:
         self.WIN = 0
         self.CLOCK = 0
 
-        # Цвет фона
         self.BGCOLOR = self.DARKBLUE
 
-        self.SPEED_CARDS = 8     # Скорость открытия/закрытия карточек
-        self.BOX_SIZE = 40      # Размер одной карточки в пикселях
-        self.INDENTS = 10    # Отступы между карточками
+        self.SPEED_CARDS = 8    
+        self.BOX_SIZE = 40     
+        self.INDENTS = 10   
 
-        self.COUNTS_CARDS_X = 2  # Количество карточек по горизонтали
-        self.COUNTS_CARDS_Y = 2    # Количество карточек по вертикали
+        self.COUNTS_CARDS_X = 2  
+        self.COUNTS_CARDS_Y = 2    
 
-        self.BOX_COLOR = self.ROSE   # Цвет для коробочек
+        self.BOX_COLOR = self.ROSE   
         
-        # Формы иконок
         self.DISK = 'disk'
         self.SQUARE = 'square'
         self.SQUARE_WITH_CIRCLE = 'circle'
         self.TRIANGLE = 'triangle'
         self.HEART = 'heart'
-        
-        # Настройки для кнопок      
+          
         self.button_width = 100
         self.button_height = 50
         self.button_color = (0, 255, 0)
         self.button_text_color = (255, 255, 255)
 
-        # Настройки для окна ввода
         self.input_box = pygame.Rect(self.WIN_WIDTH // 2.75, 250, 200, 40)
         self.input_color_inactive = pygame.Color('lightskyblue3')
         self.input_color_active = pygame.Color('dodgerblue2')
         self.input_text = ''
         self.input_active = False
 
-        # Настройки шрифтов
         pygame.font.init()
         self.font = pygame.font.SysFont(None, 36)
         self.button_font = pygame.font.SysFont(None, 30)
         self.input_font = pygame.font.SysFont(None, 40)
 
-        # Список цветов
         self.COLORS = (self.LIGHTBLUE, self.YELLOW, self.SKINCOLOR, self.PURPLE, self.MINT, self.CORAL, self.GREEN)
-        # Список форм иконок
         self.SHAPES = (self.DISK, self.SQUARE, self.SQUARE_WITH_CIRCLE, self.TRIANGLE, self.HEART)
 
-        # Проверка на четное количество карточек
         assert(self.COUNTS_CARDS_X*self.COUNTS_CARDS_Y) % 2 == 0, 'Error'   
-        # Высчитываем по Х где рисовать доску, чтобы она была посередине
         self.X_INDENTS = int((self.WIN_WIDTH - (self.COUNTS_CARDS_X * (self.BOX_SIZE + self.INDENTS)))/2)    
-        # Высчитываем по Y где рисовать доску, чтобы она была посередине
         self.Y_INDENTS = int((self.WIN_HEIGHT - (self.COUNTS_CARDS_Y * (self.BOX_SIZE + self.INDENTS))) / 2)     
 
-        # Проверка чтобы кол-во карточек на доске не привышало кол-во возможных комбинаций цвет-форма иконок
         assert len(self.COLORS) * len(self.SHAPES) * 2 >= self.COUNTS_CARDS_X * self.COUNTS_CARDS_Y, "Error"
 
         self.main()
@@ -89,12 +77,9 @@ class Game:
     def main(self):
         pygame.init()
         
-        # Музыка
         dir = path.join(path.dirname(__file__), 'music_dir')
         pygame.mixer.music.load(path.join(dir, 'music.mp3'))
-        # Параметр -1 для зацикливания фоновой фузыки
         pygame.mixer.music.play(-1)
-        # Решулируем громкость фоновой музыки
         pygame.mixer.music.set_volume(0)
 
         self.WIN = pygame.display.set_mode((self.WIN_WIDTH,self.WIN_HEIGHT))  
@@ -106,13 +91,11 @@ class Game:
 
         click_on_the_first_card = None
 
-        # Запуск начальных функций
         board = self.random_board() 
         open_boxes = self.generate_open_boxes(False)
 
         self.WIN.fill(self.BGCOLOR) 
 
-        # Надпись об уровне
         self.create_mes(f'Уровень: {self.LVL}', self.ROSE, 10,0,'comicsans', 25)
         self.create_mes(f'Шагов осталось: {self.STEP}', self.ROSE,230,0,'comicsans', 25)
         self.create_mes(f'Счет: {self.SCORE}', self.ROSE,540,0,'comicsans', 25)
@@ -122,7 +105,6 @@ class Game:
         while True:    
             mouse_clicked = False
             self.WIN.fill(self.BGCOLOR) 
-            # Вызываем функцию отрисовки доски
             self.draw_board(board, open_boxes)
 
             interrupt_button_rect = pygame.Rect(self.WIN_WIDTH // 2 - self.button_width // 2, 400, self.button_width, self.button_height)
@@ -136,16 +118,13 @@ class Game:
             self.create_mes(f'Шагов осталось: {self.STEP}', self.ROSE,220,0,'comicsans', 25)
             self.create_mes(f'Счет: {self.SCORE}', self.ROSE,540,0,'comicsans', 25)
 
-            # Цикл обработки событий
             for event in pygame.event.get():
-                # Теперь при нажатии на esc будет происходить закрытие окна
                 if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                     pygame.quit()
                     sys.exit()
 
                 elif event.type == MOUSEMOTION:
                     pos_mouse_x, pos_mouse_y = event.pos
-                    
 
                 elif event.type == MOUSEBUTTONUP:
                     pos_mouse_x, pos_mouse_y = event.pos
@@ -162,31 +141,25 @@ class Game:
 
             card_pos_mouse_x, card_pos_mouse_y = self.card_definition(pos_mouse_x, pos_mouse_y)
             
-            # Если установлены координаты карточки, на которую указывает курсор
             if card_pos_mouse_x != None and card_pos_mouse_y != None:
 
-                # Если эта карта закрыта
                 if not open_boxes[card_pos_mouse_x][card_pos_mouse_y]:
                     self.selection_highlighting(card_pos_mouse_x, card_pos_mouse_y)
 
-                # Если эта корта закрыта и мы по ней кликнули
                 if not open_boxes[card_pos_mouse_x][card_pos_mouse_y] and mouse_clicked:
                     self.opening_cards([(card_pos_mouse_x, card_pos_mouse_y)], board)
                     open_boxes[card_pos_mouse_x][card_pos_mouse_y] = True
 
-                    # Если переменная = None, то эта первая карточка, на которую мы кликнули
                     if click_on_the_first_card == None:
                         click_on_the_first_card = (card_pos_mouse_x, card_pos_mouse_y)
 
-                    # Если в ней не None, то это вторая кликнутая карточка и мы можем сравнить иконки в них
                     else:
                         card_color_1, card_shape_1 = self.shape_and_color(board, click_on_the_first_card[0], click_on_the_first_card[1])
                         card_color_2, card_shape_2 = self.shape_and_color(board, card_pos_mouse_x, card_pos_mouse_y)
 
-                        # Если значения совпадают
                         if card_shape_1 == card_shape_2 and card_color_1 == card_color_2:
                             self.SCORE += 1
-                        # Если значения не совпадают
+
                         if card_shape_1 != card_shape_2 or card_color_1 != card_color_2:
                             self.STEP -= 1
 
@@ -195,7 +168,6 @@ class Game:
                             open_boxes[click_on_the_first_card[0]][click_on_the_first_card[1]] = False
                             open_boxes[card_pos_mouse_x][card_pos_mouse_y] = False
 
-                        # Проверяем на победу
                         elif self.victory(open_boxes):
                             self.victory_animation(board, open_boxes)
                             pygame.time.wait(1000)
@@ -205,15 +177,11 @@ class Game:
                                 self.COUNTS_CARDS_X += 2
                                 self.COUNTS_CARDS_Y += 2
                                 self.STEP = self.COUNTS_CARDS_X * self.COUNTS_CARDS_Y
-                                # Повторяем проверку что у у нас четное кол-во карт
                                 assert(self.COUNTS_CARDS_X*self.COUNTS_CARDS_Y)%2 == 0, 'Error'
 
-                                # Высчитываем отступы между окном и доской
                                 self.X_INDENTS = int((self.WIN_WIDTH - (self.COUNTS_CARDS_X * (self.BOX_SIZE + self.INDENTS))) / 2)
                                 self.Y_INDENTS = int((self.WIN_HEIGHT - (self.COUNTS_CARDS_Y * (self.BOX_SIZE + self.INDENTS))) / 2)
-                        
                             
-                            # Запускаем игру заново:
                             board = self.random_board() 
                             open_boxes = self.generate_open_boxes(False)
                             self.draw_board(board, open_boxes)
@@ -231,8 +199,6 @@ class Game:
             pygame.display.update() 
             self.CLOCK.tick(self.FPS)
 
-
-    # Функция создает структуру доски
     def random_board(self):
         img = []
         for i in self.COLORS:
@@ -254,7 +220,6 @@ class Game:
         return board
 
 
-    # Функция задает стартовое знаечние ячейкам - все они закрыты
     def generate_open_boxes(self, flag):    
         open_boxes = []
         for _ in range(self.COUNTS_CARDS_X): 
@@ -262,7 +227,6 @@ class Game:
         
         return open_boxes
 
-    # Функция, которая рисует доску
     def draw_board(self, board, boxes):
         for i in range(self.COUNTS_CARDS_X):
             for j in range(self.COUNTS_CARDS_Y):
@@ -275,21 +239,15 @@ class Game:
                     shape, color = self.shape_and_color(board, i, j)
                     self.draw_icon(color, shape, i, j)
                     
-
-
-    # Функция, возвращающая левую верхнюю координату карточки
     def left_top_coord(self, i, j):
         left = i * (self.BOX_SIZE + self.INDENTS) + self.X_INDENTS
         top = j * (self.BOX_SIZE + self.INDENTS) + self.Y_INDENTS 
         
         return (left, top)
 
-    # Функция, возвращающая цвет и форму карточки
     def shape_and_color(self, board, i, j):
-        # В board обращаемся к нужной ячейке, в ней кортеж (shape, color)  
         return board[i][j][0], board[i][j][1]
-    
-    #  Функция отрисовки иконок
+
     def draw_icon(self, shape, color, i, j):
         left, top = self.left_top_coord(i, j) 
         
@@ -314,14 +272,16 @@ class Game:
             pygame.draw.polygon(self.WIN, color, [[left+20, top+5], [left+5, top+35], [left+35, top+35]])
             pygame.draw.polygon(self.WIN, self.ROSE, [[left+20, top+5], [left+12, top+20], [left+28, top+20]])
             pygame.draw.polygon(self.WIN, self.BGCOLOR, [[left+20, top+10], [left+15, top+25], [left+25, top+25]])
-            
+            pygame.draw.line(self.WIN, color, (left + 5, top + 35), (left + 35, top + 35), 3)
+
         elif shape == self.HEART:
-            pygame.draw.polygon(self.WIN, color, [[left+20, top+35], [left+5, top+20], [left+5, top+15], [left+10, top+10], [left+15, top+10], [left+20, top+16], [left+25, top+10], [left+30, top+10], [left+35, top+15], [left+35, top+20]])
+            pygame.draw.polygon(self.WIN, color, [[left+20, top+35], [left+5, top+20], [left+5, top+15], [left+10, top+10], \
+                                                  [left+15, top+10], [left+20, top+16], [left+25, top+10], [left+30, top+10], \
+                                                    [left+35, top+15], [left+35, top+20]])
             pygame.draw.polygon(self.WIN, self.BGCOLOR, [[left+20, top+35], [left+10, top+20], [left+30, top+20]])
             pygame.draw.line(self.WIN, color, (left + 5, top + 20), (left + 35, top + 20), 3)
 
 
-    # Функция анимации открытия карточек в начале уровня
     def opening_cards_at_the_beginning(self, board):
         dop_boxes = []
 
@@ -337,8 +297,6 @@ class Game:
             self.opening_cards(i,board)
             self.closing_cards(i, board)
 
-
-    # Функция группировки
     def grouping_cards(self, num, dop_boxes):
         groups = []
 
@@ -346,19 +304,16 @@ class Game:
             groups.append(dop_boxes[i:i+num])
 
         return groups
-        
-    # Функция открытия карточек    
+           
     def opening_cards(self, grouping,board):
         for i in range(self.BOX_SIZE, -1, -self.SPEED_CARDS):
             self.game_board(board, grouping, i)
 
 
-    # Функция закрытия карточек
     def closing_cards(self, grouping, board):
         for i in range(0, self.BOX_SIZE+1, self.SPEED_CARDS):
             self.game_board(board, grouping, i)
 
-    # Функция доски, отрисовывающая открытие/закрытия
     def game_board(self, board, grouping, closing_on_pixels):
         
         for i in grouping:
@@ -375,7 +330,6 @@ class Game:
         pygame.display.update()
         self.CLOCK.tick(self.FPS)
 
-    # Функция, определяющая над какой карточкой находится курсор
     def card_definition(self, pos_mouse_x, pos_mouse_y):
         
         for i in range(self.COUNTS_CARDS_X):
@@ -390,12 +344,10 @@ class Game:
         return (None, None)
 
 
-    # Функция, отрисовывающая обводку выбранной карточки
     def selection_highlighting(self, x, y):
         left, top = self.left_top_coord(x,y)
         pygame.draw.rect(self.WIN, self.ROSE, (left-5, top-5, self.BOX_SIZE+10, self.BOX_SIZE+10), 5)
 
-    # Функция определения победы
     def victory(self, open_boxes):
         for i in open_boxes:
             if False in i:
@@ -403,7 +355,6 @@ class Game:
 
         return 1
 
-    # Функция анимации при победе
     def victory_animation(self, board, board_with_open_boxes):
         color_1 = self.DARKBLUE
         color_2 = self.GRAY
@@ -416,21 +367,16 @@ class Game:
             pygame.display.update()
             pygame.time.wait(250)
 
-   
         
-    # Функция сохранения статистики лидеров
     def save_leaderboard(self, scores):
-        # Сортировка счетов по убыванию
         scores.sort(key=lambda x: x[1], reverse=True)
 
-        # Сохранение только первых 8 результатов
-        top_8_scores = scores[:8]
+        top_10_scores = scores[:8]
 
         with open("leaderboard.txt", "w") as file:
-            for score in top_8_scores:
+            for score in top_10_scores:
                 file.write(f"{score[0]}: {score[1]}\n")
 
-    # Функция считывания данных из файла
     def load_leaderboard(self):
         scores = []
         file_path = os.path.join(os.path.dirname(__file__), "leaderboard.txt")
@@ -445,14 +391,11 @@ class Game:
 
         return scores
 
-    
-    # Функция отображения и навигации при приостановке/проигрыше
     def display_leaderboard(self):
         scores = self.load_leaderboard()
         self.WIN.fill(self.BGCOLOR)
 
         text_y = 50
-        # Вывод файла
         title_text = self.font.render("ТОП-8 лучших результатов", True, self.ROSE)
         title_rect = title_text.get_rect(center=(self.WIN_WIDTH // 2, 20))
         self.WIN.blit(title_text, title_rect)
@@ -463,7 +406,6 @@ class Game:
             self.WIN.blit(score_text, score_rect)
             text_y += 30
 
-        # Навигация
         self.create_mes(f'Хочешь занести себя в таблицу лидеров?', self.ROSE, 80,300,'comicsans', 25)
         self.create_mes(f'Твой счет: {self.SCORE}', self.ROSE, 240,330,'comicsans', 25)
 
@@ -503,7 +445,7 @@ class Game:
                         self.input_text = self.get_user_name()
                         new_score = self.SCORE  
                         scores.append((self.input_text, new_score))
-                        scores.sort(key=lambda x: x[1], reverse=True)  # Сортировка по счету
+                        scores.sort(key=lambda x: x[1], reverse=True) 
                         self.save_leaderboard(scores)
                         enter_flag = 0
                         wait_for_key_press = False
@@ -520,12 +462,9 @@ class Game:
                         self.COUNTS_CARDS_Y = 2 
                        
                         assert(self.COUNTS_CARDS_X*self.COUNTS_CARDS_Y) % 2 == 0, 'Error'   
-                        # Высчитываем по Х где рисовать доску, чтобы она была посередине
                         self.X_INDENTS = int((self.WIN_WIDTH - (self.COUNTS_CARDS_X * (self.BOX_SIZE + self.INDENTS)))/2)    
-                        # Высчитываем по Y где рисовать доску, чтобы она была посередине
                         self.Y_INDENTS = int((self.WIN_HEIGHT - (self.COUNTS_CARDS_Y * (self.BOX_SIZE + self.INDENTS))) / 2)     
 
-                        # Проверка чтобы кол-во карточек на доске не привышало кол-во возможных комбинаций цвет-форма иконок
                         assert len(self.COLORS) * len(self.SHAPES) * 2 >= self.COUNTS_CARDS_X * self.COUNTS_CARDS_Y, "Error"
 
                         wait_for_key_press = False
@@ -537,12 +476,11 @@ class Game:
             self.display_leaderboard()
 
     
-    # Функция ввода имени пользователя
     def get_user_name(self):
         self.input_active = True
         while self.input_active:
             for event in pygame.event.get():
-                if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+                if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
 
@@ -561,7 +499,6 @@ class Game:
             self.draw_input_box()
             pygame.display.flip()
         
-    # Функция отрисовки интерфейса для ввода
     def draw_input_box(self):
 
         if self.input_active:
@@ -572,7 +509,6 @@ class Game:
         pygame.draw.rect(self.WIN, color, self.input_box, 2)
 
         self.create_mes('Введите свое имя', self.ROSE, 220, 150, 'comicsans', 25)
-
         text = self.input_font.render(self.input_text, True, (255, 255, 255))
         width = max(200, text.get_width()+10)
         input_box_rect = pygame.Rect(self.WIN_WIDTH // 2.75, 250, width, 40)
@@ -584,14 +520,11 @@ class Game:
             cursor_y = input_box_rect.y + 5
             pygame.draw.line(self.WIN, (255, 255, 255), (cursor_x, cursor_y), (cursor_x, cursor_y + 30), 2)
 
-
-     # Функция отрисовки надписи
     def create_mes(self, msg, color, x,y, font, size):
         font_style = pygame.font.SysFont(font, size)
         mesg = font_style.render(msg, True, color)
         self.WIN.blit(mesg, [x, y])
 
- # Запускаем главную функцию игры
 game = Game()
     
 
